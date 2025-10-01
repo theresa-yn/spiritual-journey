05c3f1790e5792ca6d38791bfb9bca38a83186dd
Theresa Nguyen <littlesheep@Mac.lan>
2025-09-30 21:31:59 -0400

    Update service worker cache version to force refresh of updated title

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker for Soul Reflections App
-const CACHE_NAME = "soul-reflections-cache-v1";
+const CACHE_NAME = "soul-reflections-cache-v2";
 const OFFLINE_URL = "offline.html";
 // Files to cache
 const FILES_TO_CACHE = [
